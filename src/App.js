135993f64ec703c29e0fe377b6c1b02dda1614e4
Raj-abm <84534626+Raj-abm@users.noreply.github.com>
2023-10-28 16:47:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const backendUrl = 'http://localhost:1338'
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      fetch(backendUrl+'/files/upload', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => {
+
+        })
+        .catch((error) => {
+        });
+    }
+  };
+  const handleDelete = (fileName) => {
+    console.log('DELETING FILE',fileName)
+    fetch(backendUrl+`/files/delete/${fileName}`, {
+      method: 'DELETE',
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        setFiles((prevFileList) => prevFileList.filter((file) => file !== fileName));
+      }
+    })
+    .catch((error) => {
+      console.error('Error deleting file: ', error);
+    });
+  };
+
+  
+  const deletefile = (fileName) => {
+    console.log('DELETING FILE',fileName)
+    fetch(backendUrl+`/files/delete/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json' // Set the content type to JSON
+      },
+      body:JSON.stringify({fileName})
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        setFiles((prevFileList) => prevFileList.filter((file) => file !== fileName));
+      }
+    })
+    .catch((error) => {
+      console.error('Error deleting file: ', error);
+    });
+  };
+
+  useEffect(()=>{
+    fetch('http://localhost:1338/files/list').then(res=>res.json()).then(data=>{
+      setFiles(data)
+    })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <div>
+        <input type='file' onChange={handleFileChange}/>
+        <button onClick={handleUpload}>Upload</button>
+      </div>
+      <br />
+      <div>
+        Files
+      </div>
+      <div>
+        <ol>
+          {files.map((file,i)=>{
+            return <li key={i}>{file} <button onClick={() => deletefile(file)}>Delete</button></li>
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
